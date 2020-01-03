d130c423f9b4c2f7fb4a654db4f91eea6f76c4ca
bukinoshita <bukinoshita@gmail.com>
2020-01-03 16:50:59 -0300

    refactor: add default path

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,4 @@
 import { existsSync } from 'fs'
 import { resolve } from 'path'
 
-export default (cwd?: string) => existsSync(resolve(cwd || process.cwd(), 'package.json'))
+export default (cwd = process.cwd()) => existsSync(resolve(cwd, 'package.json'))
